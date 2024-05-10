53376e22fa0005e5c9b957ed25c76ea9e6a5da47
KILLER-147 <161155790+KILLER-147@users.noreply.github.com>
2024-05-10 21:09:09 +0100

    Update HExX-XD.py

--- HExX-XD.py
+++ HExX-XD.py
@@ -4,6 +4,6 @@ os.system('git pull')
 os.system("clear")
 hexx=platform.architecture()[0]
 if hexx=="32bit":
-    __import__("nn")
+    __import__("H11")
 elif hexx=="64bit":
-    __import__("nn")
+    __import__("H11")
