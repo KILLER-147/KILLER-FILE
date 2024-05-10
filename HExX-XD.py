04139f27ffb33611e87805879ca30ab88564fcf3
KILLER-147 <161155790+KILLER-147@users.noreply.github.com>
2024-05-10 20:59:56 +0100

    Update HExX-XD.py

--- HExX-XD.py
+++ HExX-XD.py
@@ -5,12 +5,12 @@ try:
  import requests
 except:os.system("pip uninstall requests -y;pip install requests")
  
-print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
+print('\033[1;97m[\033[1;97m•\033[1;97m] \033[1;97mChecking For Update...')
 os.system('git pull --quiet 2>/dev/null')
 hexxxd = platform.architecture()[0]
 if hexxxd == '64bit':
- print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit')
+ print('\033[1;97m[\033[1;97m•\033[1;97m] \033[1;97mYour Device is 64bit')
  import nn
 elif hexxxd == '32bit':
- print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Devive is 32bit')
+ print('\033[1;97m[\033[1;97m•\033[1;97m] \033[1;97mYour Devive is 32bit')
  import nn
