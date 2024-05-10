53e6ff72238577852850718baa2ae4bb02b39b87
KILLER-147 <161155790+KILLER-147@users.noreply.github.com>
2024-05-10 20:56:23 +0100

    Update HExX-XD.py

--- HExX-XD.py
+++ HExX-XD.py
@@ -1,4 +1,16 @@
 #Developer-HExX-XD#
-import os
-import nn
-nn.loging()
+import os, platform, time, sys
+os.system('pkg install espeak -y')
+try:
+ import requests
+except:os.system("pip uninstall requests -y;pip install requests")
+ 
+print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
+os.system('git pull --quiet 2>/dev/null')
+hexxxd = platform.architecture()[0]
+if hexxxd == '64bit':
+ print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit')
+ import nn
+elif hexxxd == '32bit':
+ print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Devive is 32bit')
+ import nn
